bd7f7f3c4e65d128e1d85cc1d0e532fb0ff45842
Ligia Zanchet <valar@Ligias-MacBook-Pro.local>
2026-04-02 21:33:36 +0200

    fix(kilo-docs): add VideoEmbed component and video markdoc tag

--- packages/kilo-docs/markdoc/tags/index.ts
+++ packages/kilo-docs/markdoc/tags/index.ts
@@ -5,5 +5,6 @@ export * from "./tabs.markdoc"
 export * from "./icon.markdoc"
 export * from "./image.markdoc"
 export * from "./kilo-code-icon.markdoc"
+export * from "./video.markdoc"
 export * from "./youtube.markdoc"
 
